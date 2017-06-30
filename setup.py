5b080586296e03564b2780400a120d359d9d73e9
Bjoern Riemer <bjoern.riemer@fokus.fraunhofer.de>
2017-06-30 10:39:11 +0200

    increase version to 1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="sdn-manager",
-    version="0.1.1",
+    version="1.0.0",
     author="SoftFIRE",
     author_email="softfire@softfire.eu",
     description="The SoftFIRE SDN Manager",
