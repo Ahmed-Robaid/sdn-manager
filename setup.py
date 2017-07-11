9d7e13276b8be520844526744f29d5e32d6847e3
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-07-11 15:56:28 +0200

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="sdn-manager",
-    version="1.0.0",
+    version="1.0.1",
     author="SoftFIRE",
     author_email="softfire@softfire.eu",
     description="The SoftFIRE SDN Manager",
