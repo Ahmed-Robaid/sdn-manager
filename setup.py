28debc895e208f1a3eed3e68582780f8586b9fb5
Thomas Briedigkeit <thomas.briedigkeit@fokus.fraunhofer.de>
2017-09-07 15:22:56 +0200

    Update version to 1.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="sdn-manager",
-    version="1.0.1",
+    version="1.0.2",
     author="SoftFIRE",
     author_email="softfire@softfire.eu",
     description="The SoftFIRE SDN Manager",
